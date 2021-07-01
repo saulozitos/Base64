ed1c9b97a2c33aa2a777ee3ac718517a4a7e04ed
Saulo Campos <saulo.campos@stone.com.br>
2021-07-01 11:39:38 -0300

    add teste

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "base64.h"
+#include <iostream>
+
+int main() {
+
+  Base64 base64;
+  const std::string message = {"This is a menssage to be encoded!"};
+  const std::string codedMessage = base64.encode(message);
+
+  std::cout << "Coded message: " << codedMessage << '\n';
+
+  const std::string decodedMessage = base64.decode(codedMessage);
+
+  std::cout << "Decoded message: " << decodedMessage << '\n';
+  return 0;
+}
